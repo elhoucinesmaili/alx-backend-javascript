ebf9baaef7072d44a9507c62988d121876d94a6d
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-04-17 15:09:21 +0000

    git

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,33 @@
-// This class defines a Currency with a code and name, and includes a method to display full currency info
+// Define and export the Currency class
 export default class Currency {
+  // Constructor initializes currency code and name
   constructor(code, name) {
     this._code = code;
     this._name = name;
   }
 
+  // Getter for currency code
   get code() {
     return this._code;
   }
 
+  // Setter for currency code
   set code(n) {
     this._code = n;
   }
 
+  // Getter for currency name
   get name() {
     return this._name;
   }
 
+  // Setter for currency name
   set name(n) {
     this._name = n;
   }
 
+  // Method to display full currency in "Name (Code)" format
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
-

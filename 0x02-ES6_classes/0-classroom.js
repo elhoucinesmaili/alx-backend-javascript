88a1a7e4b1fc4997e38ffa1aeff7f5905cf251d2
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-04-17 14:40:34 +0000

    commit

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+// This class represents a classroom with a maximum number of students allowed
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
